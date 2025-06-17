7a4551a8c8b9bb22f13f2e89282da2eea14786f0
root <root@ip-172-31-88-183.ec2.internal>
2025-06-17 18:54:16 +0000

    new Locals workspace

---
+++ Terraform/Workspace/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "main" {
+    ami = var.ami_id
+    instance_type = local.instance_type
+
+    tags = {
+        name = "ec2 $(terraform.workspace)"
+        Environment = terraform.workspace
+    }
+}
+
+locals {
+    instance_type_map {
+        dev = "t2.micro"
+        stage = "t3.micro"
+        prod = "t2.small"
+    }
+
+    instance_type = lookup (local.instance_type_map, terrafrom.workspace, "t2.micro")
+}
