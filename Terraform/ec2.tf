9b4ec69d7c3f352d6a172879877f793fd77ba407
Aditya Mane <121067242+Adyaa12@users.noreply.github.com>
2025-06-17 12:59:33 +0530

    Update ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "my-instance" {
   ami = "ami-020cba7c55df1f615"
   instance_type = var.instance_type
-  vpc_security_group_id  = aws_vpc.main.id
+  vpc_security_group_ids  = aws_vpc.main.id
   subnet_id = aws_subnet.subnet1.id
 
   tags =  {
@@ -25,7 +25,7 @@ resource "aws_instance" "my-instance" {
 resource "aws_instance" "my-instance2" {
   ami = "ami-020cba7c55df1f615"
   instance_type = var.instance_type
-  vpc_security_group_id  = aws_vpc.main.id
+  vpc_security_group_ids  = aws_vpc.main.id
   subnet_id = aws_subnet.subnet1.id
 
   tags =  {
